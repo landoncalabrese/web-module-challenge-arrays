9130ab92ddabfcead52c09d7c88bc79f3ffdb15a
landoncalabrese <landoncalabrese@gmail.com>
2022-02-09 18:13:28 -0500

    finished

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ function removeFlavorByName(array, flavor){
       array.splice(i, 1);
     }
   }
-  return array
+  return array;
 }
 console.log('task 6:', removeFlavorByName(originalFlavors, "Rocky Road"));
 
@@ -172,10 +172,16 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(array, flavor){
+let filteredArray = [];
+for(let i = 0; i < array.length; i++){
+  if(array[i].includes(flavor)){
+    filteredArray.push(array[i]);
+  }
 }
-
+return filteredArray;
+}
+console.log('task 7:', filterByWord(originalFlavors, 'Chocolate'));
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
